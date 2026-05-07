0b02b7bdba845df47f367f63792ee94074a2a873
akachammile <1224307033@qq.com>
2026-05-07 19:16:23 +0800

    chore(agent): remove deleted DeepAgent and DeepSearchAgent modules

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,6 +1,5 @@
 from .common import BaseAgent
-from .deepagents import DeepAgent
 from .designagent import DesignAgent
 from .manager import agent_manager
 
-__all__ = ["BaseAgent", "DeepAgent", "DesignAgent", "agent_manager"]
+__all__ = ["BaseAgent", "DesignAgent", "agent_manager"]
