32831849c4b33acee26e24ce11211801fb5f3bb3
akachammile <1224307033@qq.com>
2026-07-23 04:35:51 -0800

    fix(sandbox): 补充沙箱中间件模块导出注册

--- src/agents/middlewares/__init__.py
+++ src/agents/middlewares/__init__.py
@@ -1,6 +1,7 @@
 from .attachment_middleware import AttachmentMiddleware
 from .call_limit_middleware import create_call_limit_middlewares
 from .human_in_loop_middleawre import HumanInLoopMiddleware
+from .sandbox_middleware import SandboxMiddleware
 from .search_middleware import SearchToolMiddleware
 from .subagent_middlware import SubAgentMiddleware, create_subagent_middleware
 from .summary_middleware import create_summary_middleware
@@ -9,6 +10,7 @@ from .token_usage_middleware import TokenUsageMiddleware
 __all__ = [
     "AttachmentMiddleware",
     "HumanInLoopMiddleware",
+    "SandboxMiddleware",
     "SearchToolMiddleware",
     "SubAgentMiddleware",
     "TokenUsageMiddleware",
